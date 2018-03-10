5a61a195583eed351758b9e1d93fbf4599603e14
pawelpawel8787 <bassista@interia.pl>
2018-03-10 20:28:50 +0100

    MainTvpHistoria - add regex which delete round brackets from date

--- src/main/java/com/mycompany/MainTvpHistoria.java
+++ src/main/java/com/mycompany/MainTvpHistoria.java
@@ -12,6 +12,7 @@ public class MainTvpHistoria {
         String path = "E:\\IdeaProjects\\ProgramObrobkaPlikowTV\\documents\\input\\TVP Historia.txt";
         String pathOut = "E:\\IdeaProjects\\ProgramObrobkaPlikowTV\\documents\\output\\outputTVP Historia1.txt";
         String pathOut2 = "E:\\IdeaProjects\\ProgramObrobkaPlikowTV\\documents\\output\\outputTVP Historia2.txt";
+        String pathOut3 = "E:\\IdeaProjects\\ProgramObrobkaPlikowTV\\documents\\output\\outputTVP Historia3.txt";
         BufferedReader reader = null;
         BufferedWriter writer = null;
 
@@ -58,20 +59,60 @@ public class MainTvpHistoria {
 
             while ((line2 = reader.readLine()) != null) {
 
-                Pattern pattern = Pattern.compile("odc. ([0-9]{1,3})/([0-9]{1,3})");
+                Pattern pattern = Pattern.compile("odc. ([0-9]{1,3})/([0-9]{1,3}) ");
+                Pattern pattern2 = Pattern.compile("odc. ([0-9]{1,3}) ");
+
                 Matcher matcher = pattern.matcher(line2);
+                Matcher matcher2 = pattern2.matcher(line2);
 
                 if (matcher.find()) {
-                    System.out.println(matcher.group());
                     writer.write(line2
                             .replace(matcher.group(), "")
+                    );
+                } else if (matcher2.find()) {
+                    writer.write(line2
+                            .replace(matcher2.group(), "")
                     );
                 } else {
                     writer.write(line2
                             .replace("", ""));
-
                 }
+                writer.append("\n");
+            }
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
+            if (writer != null) {
+                writer.close();
+            }
+        }
+
+        try {
+            reader = new BufferedReader(new FileReader(pathOut2));
+            writer = new BufferedWriter(new FileWriter(pathOut3));
+
+            String line3;
+
+            while ((line3 = reader.readLine()) != null) {
+                    // szuka stringow (1), (12), (123) lub (1234)
+                Pattern pattern = Pattern.compile("\\(([0-9]{1,4})\\)");
+                Matcher matcher = pattern.matcher(line3);
+
+                if (matcher.find()) {
+                    String foundMatcher = matcher.group();
+                    //  zamienia znalezione stringi np. (2012) na 2012
+                    writer.write(line3
+                            .replace(foundMatcher, foundMatcher.substring(1, matcher.group().length() - 1))
+                    );
+                }
+                 else {
+                    writer.write(line3
+                            .replace("", ""));
+                }
                 writer.append("\n");
             }
 
