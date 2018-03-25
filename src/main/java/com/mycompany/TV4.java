1bbe56bac743217f33c975d1fb794ec5905cf5e6
pawelpawel8787 <bassista@interia.pl>
2018-03-25 23:02:08 +0200

    TV4 - almost ready

--- src/main/java/com/mycompany/TV4.java
+++ src/main/java/com/mycompany/TV4.java
@@ -37,15 +37,102 @@ public class TV4 {
                 if (line1.contains("Detektywi w Akcji")) {
                     writer.write(line1
                             .replace(line1, line1.substring(0, 5) + "\t" + "Detektywi w akcji - serial kryminalny prod. Polska")
-
-
                     );
-//                } else if (line1.contains("MiłośćKropka.pl")) {
-//                    writer.write(line1
-//                            .replace(line1, line1.substring(0, 5) + "\t" + "MiłośćKropka.pl - rozrywkowa")
-//                    );
-//                }
-                } else {
+
+                } else if (line1.contains("Jeźdźcy smoków,")) {
+                    writer.write(line1
+                            .replace(line1, line1.substring(0, 5) + "\t" + "Jeźdźcy smoków")
+                    );
+                } else if (line1.contains("Dragons: Defenders of Berk")) {
+                    writer.write(line1
+                            .replace(line1, line1.substring(0, 5) + "\t" + "Dragons: Defenders of Berk")
+                    );
+                } else if (line1.contains("Drużyna A")) {
+                    writer.write(line1
+                            .replace(line1, line1.substring(0, 5) + "\t" + "Drużyna A")
+                    );
+                } else if (line1.contains("Detektyw Monk")) {
+                    writer.write(line1
+                            .replace(line1, line1.substring(0, 5) + "\t" + "Detektyw Monk - serial kryminalny")
+                    );
+                } else if (line1.contains("Sekrety Sąsiadów")) {
+                    writer.write(line1
+                            .replace(line1, line1.substring(0, 5) + "\t" + "Sekrety Sąsiadów")
+                    );
+                } else if (line1.contains("Galileo")) {
+                    writer.write(line1
+                            .replace(line1, line1.substring(0, 5) + "\t" + "Galileo")
+                    );
+                } else if (line1.contains("Nie igraj z aniołem")) {
+                    writer.write(line1
+                            .replace(line1, line1.substring(0, 5) + "\t" + "Nie igraj z aniołem - telenowela")
+                    );
+                } else if (line1.contains("Policjantki i Policjanci")) {
+                    writer.write(line1
+                            .replace(line1, line1.substring(0, 5) + "\t" + "Policjantki i Policjanci - serial obyczajowy prod. Polska")
+                    );
+                } else if (line1.contains("Czasy ostateczne")) {
+                    writer.write(line1
+                            .replace(line1, line1.substring(0, 5) + "\t" + "Czasy ostateczne: Pozostawieni")
+                    );
+                } else if (line1.contains("Sprawiedliwi")) {
+                    writer.write(line1
+                            .replace(line1, line1.substring(0, 5) + "\t" + "Sprawiedliwi - Wydział Kryminalny")
+                    );
+                } else if (line1.contains("Na Patrolu")) {
+                    writer.write(line1
+                            .replace(line1, line1.substring(0, 5) + "\t" + "Na Patrolu")
+                    );
+                } else if (line1.contains("Jeźdźcy smoków na końcu świata,")) {
+                    writer.write(line1
+                            .replace(line1, line1.substring(0, 5) + "\t" + "Jeźdźcy smoków na końcu świata - serial przygodowy")
+                    );
+                } else if (line1.contains("Disco Polo Life")) {
+                    writer.write(line1
+                            .replace(line1, line1.substring(0, 5) + "\t" + "Disco Polo Life")
+                    );
+                } else if (line1.contains("Top 10 lista przebojów")) {
+                    writer.write(line1
+                            .replace(line1, line1.substring(0, 5) + "\t" + "Top 10 lista przebojów")
+                    );
+                } else if (line1.contains("Tom i Jerry Show,")) {
+                    writer.write(line1
+                            .replace(line1, line1.substring(0, 5) + "\t" + "Tom i Jerry Show")
+                    );
+                } else if (line1.contains("STOP Drogówka")) {
+                    writer.write(line1
+                            .replace(line1, line1.substring(0, 5) + "\t" + "STOP Drogówka")
+                    );
+                } else if (line1.contains("Mecenas Lena Barska,")) {
+                    writer.write(line1
+                            .replace(line1, line1.substring(0, 5) + "\t" + "Mecenas Lena Barska")
+                    );
+                } else if (line1.contains(" - Polska, 2018 ")) {
+                    writer.write(line1
+                            .replace(line1, "")
+                    );
+                } else if (line1.contains(" - Polska - ")) {
+                    writer.write(line1
+                            .replace(line1, "")
+                    );
+                } else if (line1.contains("Cafe Futbol")) {
+                    writer.write(line1
+                            .replace(line1, line1.substring(0, 5) + "\t" + "Cafe Futbol")
+                    );
+                }
+                else if (line1.contains("Anthony Bourdain")) {
+                    writer.write(line1
+                            .replace(line1, line1.substring(0, 5) + "\t" + "Anthony Bourdain")
+                    );
+                }
+                else if (line1.contains(" i audiodeskrypcja")) {
+                    writer.write(line1
+                            .replace(line1, "")
+                    );
+                }
+
+
+                else {
                     writer.write(line1
                             .replace("", ""));
                 }
@@ -64,128 +151,32 @@ public class TV4 {
         }
 
         try {
-            reader2 = new Scanner(new File(pathOut));
-            writer2 = new PrintWriter(pathOut2);
-
-
-            while ((reader2.hasNext())) {
-                String line2 = reader2.nextLine();
-
-                if (!line2.isEmpty()) {
-                    writer2.write(line2);
-                    writer2.write("\n");
-                }
-
-
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (reader != null) {
-                reader.close();
-            }
-            if (writer != null) {
-                writer.close();
-            }
-        }
-
-        try {
-            reader = new BufferedReader(new FileReader(pathOut2));
-            writer = new BufferedWriter(new FileWriter(pathOut3));
-
-            String line2;
-
-            while ((line2 = reader.readLine()) != null) {
-                writer.write(line2.trim().replace("\n", ""));
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (reader != null) {
-                reader.close();
-            }
-            if (writer != null) {
-                writer.close();
-            }
-        }
-
-        try {
-            reader = new BufferedReader(new FileReader(pathOut3));
-            writer = new BufferedWriter(new FileWriter(finalOut));
+            reader = new BufferedReader(new FileReader(pathOut));
+            writer = new BufferedWriter(new FileWriter(pathOut2));
 
             String line3;
 
             while ((line3 = reader.readLine()) != null) {
 
-
-                Pattern pattern = Pattern.compile("([0-9]{1,2}):([0-9]{1,2})");
-                Matcher matcher = pattern.matcher(line3);
- if (line3.length() > 100) {
-
-     String sentence = line3;
-
-                    String[] array = sentence.split("([0-9]{1,2}):([0-9]{1,2})");
-                    String word = array[1];
-
-                    writer.write(line3
-                            .replace(sentence, word)
-                    );
-
-                } else {
+                if (line3.length() > 200 && line3.contains("reż.") && line3.contains("wyk.")) {
                     writer.write(line3
                             .replace("", ""));
+
+                } else if (line3.length() > 200){
+
+                    writer.write(line3
+                            .replace(line3, ""));
                 }
 
-//                if (matcher.find()) {
-//                    writer.write(line3
-//                            .replace(matcher.group(), matcher.group() + "\n")
-//                            .replace(" fabularny, dramat, thriller", " - thriller")
-//                            .replace("fabularny, thriller", "- fabularny")
-//                            .replace("fabularny, przygodowo-fantastyczny", "- przygodowo-fantastyczny")
-//                            .replace(" fabularny, komedia obyczajowa", " - komedia obyczajowa")
-//                            .replace("dokumentalny, film dokumentalny", "- film dokumentalny")
-//                            .replace("fabularny, przygodowy, sensacyjny", "- przygodowy")
-//                            .replace("fabularny, dramat historyczny", "- dramat historyczny")
-//                            .replace("fabularny, kryminalny", "- kryminalny")
-//                            .replace("fabularny, sensacyjny", "- sensacyjny")
-//                            .replace("fabularny, komedia, przygodowy", "- komedia")
-//                            .replace("fabularny, komedia romantyczna", "- komedia romantyczna")
-//                            .replace("fabularny, komedia", "- komedia")
-//                            .replace("fabularny, dramat", "- dramat")
-//                            .replace("etiuda fabularna, krótkometrażowy", "- krótkometrażowy")
-//                            .replace("fabularny, horror", "- horror")
-//                            .replace("komedia, komedia sensacyjna", "komedia sensacyjna")
-//                            .replace("  ", " ")
-//                            .replace(", -", " -")
-//                            .replace("  -", " -")
-//                    );
-//                } else {
-//                    writer.write(line3
-//                            .replace("", "")
-//                            .replace(" fabularny, dramat, thriller", " - thriller")
-//                            .replace("fabularny, thriller", "- fabularny")
-//                            .replace("fabularny, horror", "- horror")
-//                            .replace("fabularny, komedia", "- komedia")
-//                            .replace("fabularny, przygodowo-fantastyczny", "- przygodowo-fantastyczny")
-//                            .replace(" fabularny, komedia obyczajowa", " - komedia obyczajowa")
-//                            .replace("dokumentalny, film dokumentalny", "- film dokumentalny")
-//                            .replace("etiuda fabularna, krótkometrażowy", "- krótkometrażowy")
-//                            .replace("fabularny, dramat", "- dramat")
-//                            .replace("fabularny, przygodowy, sensacyjny", "- przygodowy")
-//                            .replace("fabularny, komedia romantyczna", "- komedia romantyczna")
-//                            .replace("fabularny, dramat historyczny", "- dramat historyczny")
-//                            .replace("fabularny, kryminalny", "- kryminalny")
-//                            .replace("fabularny, sensacyjny", "- sensacyjny")
-//                            .replace("fabularny, komedia, przygodowy", "- komedia")
-//                            .replace("fabularny, przygodowy", "- przygodowy")
-//                            .replace("komedia, komedia sensacyjna", "komedia sensacyjna")
-//                            .replace("  ", " ")
-//                            .replace(", -", " -")
-//                            .replace("  -", " -")
-//                    );
-//                }
+                else {
+                    writer.write(line3
+                            .replace("", "")
+                            .replace(" - US\tAnthony Bourdain", "")
+                            .replace(" - Wielka Brytania - ", "")
+
+                    );
+
+                }
                 writer.append("\n");
             }
 
@@ -199,6 +190,34 @@ public class TV4 {
                 writer.close();
             }
         }
+
+        try {
+            reader2 = new Scanner(new File(pathOut2));
+            writer2 = new PrintWriter(finalOut);
+
+
+            while ((reader2.hasNext())) {
+                String line2 = reader2.nextLine();
+
+                if (!line2.isEmpty()) {
+                    writer2.write(line2);
+                    writer2.write("\n");
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader2 != null) {
+                reader2.close();
+            }
+            if (writer2 != null) {
+                writer2.close();
+            }
+        }
+
+
+
 
 
     }
